6f5bdc2f1828fbb446fc05b83bbe9e83fe491c65
Suleyman Suleymanzade <sul_soft@hotmail.com>
2024-06-26 15:01:43 +0400

    custom serializer/deserializers for HashGenerator have been implemented

--- src/algorithms/locality_sensitive_hashing.rs
+++ src/algorithms/locality_sensitive_hashing.rs
@@ -1,6 +1,10 @@
 use std::collections::HashMap;
 use ndarray::{Array1, Array2, arr1};
 use rand::prelude::*;
+use serde::{Serialize, Deserialize, Serializer, Deserializer};
+use serde::ser::SerializeStruct;
+use serde::de::{Visitor, SeqAccess};
+use serde_json;
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -8,6 +12,41 @@ pub struct HashGenerator {
     projections: Array2<f64>,
     hash_map: HashMap<String, Vec<String>>,
 }
+
+impl Serialize for HashGenerator {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("HashGenerator", 2)?;
+        state.serialize_field("projections", &self.projections.as_slice().unwrap())?;
+        state.serialize_field("hash_map", &self.hash_map)?;
+        state.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for HashGenerator {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        struct HashGeneratorHelper {
+            projections: Vec<f64>,
+            hash_map: HashMap<String, Vec<String>>,
+        }
+
+        let helper = HashGeneratorHelper::deserialize(deserializer)?;
+        let projections = Array2::from_shape_vec((helper.projections.len() / 2, 2), helper.projections)
+            .map_err(serde::de::Error::custom)?;
+        
+        Ok(HashGenerator {
+            projections,
+            hash_map: helper.hash_map,
+        })
+    }
+}
+
 
 impl HashGenerator {
     pub fn new(hash_size: usize, inp_dimensions: usize) -> Self {
